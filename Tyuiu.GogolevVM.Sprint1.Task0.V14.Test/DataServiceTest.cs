e75e85837e7964be9c1c3f2c09bc28d0b7de604b
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-09-15 14:13:41 +0300

    Add project files.

---
+++ Tyuiu.GogolevVM.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GogolevVM.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.GogolevVM.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate;
+            Assert.AreEqual(25, res);
+
+        }
+    }
+}
