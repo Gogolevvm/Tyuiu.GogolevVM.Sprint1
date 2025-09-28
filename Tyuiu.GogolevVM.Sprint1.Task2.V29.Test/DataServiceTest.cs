1b4480a29d8b694bae6cf577ffc061d99d51ea38
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-09-28 10:38:46 +0300

    Task2V29

---
+++ Tyuiu.GogolevVM.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GogolevVM.Sprint1.Task2.V29.Lib;
+namespace Tyuiu.GogolevVM.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValueExpression()
+        {
+            DataService ds = new DataService();
+            int a = 180;
+            var res = ds.ConvertSecondsToHours(a);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
