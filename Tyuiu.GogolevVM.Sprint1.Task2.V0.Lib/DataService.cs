83312254779841478da94c4e41a439f417e3406a
Всеволод Гоголев <Tyuiu.Gogolevvm@outlook.com>
2025-09-28 09:34:49 +0300

    Task2V0

---
+++ Tyuiu.GogolevVM.Sprint1.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.GogolevVM.Sprint1.Task2.V0.Lib
+{
+    public class DataService : ISprint1Task2V0
+    {
+        public int Sqr(int value)
+        {
+            return value * value;
+        }
+    }
+}
---
+++ Tyuiu.GogolevVM.Sprint1.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GogolevVM.Sprint1.Task2.V0;
+using Tyuiu.GogolevVM.Sprint1.Task2.V0.Lib;
+namespace Tyuiu.GogolevVM.Sprint1.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int a = 2;
+            var res = ds.Sqr(a);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
